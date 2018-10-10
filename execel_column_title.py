34c40eba4e81fa4f370fe5b2314ec3d0b1b8e439
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-10-10 16:27:28 -0500

    Create execel_column_title.py

---
+++ execel_column_title.py
@@ -0,0 +1,52 @@
+# 168. Excel Sheet Column Title  
+"""
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+
+Example 1:
+Input: 1
+Output: "A"
+
+Example 2:
+Input: 28
+Output: "AB"
+
+Example 3:
+Input: 701
+Output: "ZY"
+"""
+
+"""
+Explain:
+A  1    AA  1x26+ 1   BA  2×26+ 1   ...   ZA  26×26+ 1     AAA  1×26²+1×26+ 1   BAA  
+B  2    AB  1x26+ 2   BB  2×26+ 2   ...   ZB  26×26+ 2     AAB  1×26²+1×26+ 2   BAB  
+.  .    ..  .......   ..  .......   ...   ..  ........     ...  .............   ...  
+.  .    ..  .......   ..  .......   ...   ..  ........     ...  .............   ...
+.  .    ..  .......   ..  .......   ...   ..  ........     ...  .............   ...
+Z 26    AZ  1x26+26   BZ  2×26+26   ...   ZZ  26×26+26     AAZ  1×26²+1×26+26   BAZ  
+
+example: ABCD = A×26³ + B×26² + C×26¹ + D = 1×26³ + 2×26² + 3×26¹ + 4
+n%26 method may have problem while dealing with character z:
+ZZZZ = Z×26³ + Z×26² + Z×26¹ + Z = 26×26³ + 26×26² + 26×26¹ + 26
+use (n-1)%26 instead, range from 0 to 25.
+"""
+
+# Recursive
+def convertToTitle(n):
+    if n == 0:
+        return ''
+    else:
+        convertToTitle( (n-1)//26 + chr( (n-1)%26 + ord('A') ) )
+    # ord('A') = 65
+
+
+
+# ........... not finished yet
