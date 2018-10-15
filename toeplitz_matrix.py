d5a08e0c5d78bdf3b012499df4cf11fe1a10091f
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-10-15 15:39:49 -0500

    Update toeplitz_matrix.py

--- toeplitz_matrix.py
+++ toeplitz_matrix.py
@@ -33,4 +33,15 @@ Note:
 
 """
 
+def isToepllitzMatrix(m):
+    for i in range( len(m)-1 ):
+        for j in range( len(m[0])-1 ):
+            if m[i][j] != m[i+1][j+1] :
+                return False
+    return True
 
+
+
+
+
+        
