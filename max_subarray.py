6ce7b3b4852d9891bd0f8ce8c885f840e850e68e
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-10-21 00:59:36 -0500

    Update max_subarray.py

--- max_subarray.py
+++ max_subarray.py
@@ -8,3 +8,11 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 """
 
+def maxSubArray(A):
+    currsum = maxsum = A[0]
+    for num in A[1:]:
+        currsum = max(num+currsum , num)
+        maxsum = max(currsum, maxsum)
+    return maxsum
+
+# Time O(N), Space O(2)
