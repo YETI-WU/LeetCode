dc0c21bb7cbb879273d81f45716303ab3c60f4d3
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-11-25 04:23:30 -0600

    Update longest_palindromic_substring.py

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -12,3 +12,27 @@ Example 2:
 Input: "cbbd"
 Output: "bb"
 """
+
+# Time O(N^2); Space O(1)
+def chk(s,l,r):
+  while l>0 and r<len(s) and s[l]==s[r]:
+    l += 1
+    r += 1
+    return s[l+1:r]
+  
+def longestPalindromicSubstring(s):
+  res = ''
+  for i in range(len(s)):
+    # odd 
+    tmp = chk(s,i,i)
+    if len(tmp) > len(res): 
+      res = tmp
+    # even
+    tmp = chk(s,i,i+1)
+    if len(tmp) > len(res):
+      res = tmp
+  return res
+
+
+
+
