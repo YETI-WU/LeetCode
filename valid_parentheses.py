482bf2018fd57012074e3bbbdbefcd5ba9578907
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-12-03 18:49:40 -0600

    Update valid_parentheses.py

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,6 +1,6 @@
 # 20. Valid Parentheses
 """
-Easy  Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
 determine if the input string is valid.
 An input string is valid if:
 1.	Open brackets must be closed by the same type of brackets.
