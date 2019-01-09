4fdf2a70e33d7fff74a84626b301989e96cf82e2
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-01-09 14:33:30 -0600

    Update max_area.py

--- max_area.py
+++ max_area.py
@@ -12,5 +12,17 @@ Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
 """
 
+# Time O(N); Space O(1)
+def maxArea(height):
+    i, j = 0, len(height)-1
+    water = 0
+    while i < j:
+        water = max( water, (j-i) * min(height[i], height[j]) )
+        if height[i] < height[j]:
+            i += 1
+        else:
+            j -= 1
+    return water
 
-def maxArea(
+
+  
