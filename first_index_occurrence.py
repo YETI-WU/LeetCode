9dbc0efb47ef0672f3f7d61bcc6e49284d954977
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-01-17 19:25:46 -0600

    Update first_index_occurrence.py

--- first_index_occurrence.py
+++ first_index_occurrence.py
@@ -15,3 +15,15 @@ Clarification:
 What should we return when needle is an empty string? This is a great question to ask during an interview.
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 """
+
+# Time O(N*M), Space O(1)
+def strStr(haystack, needle):
+    for i in range( len(haystack) - len(needle) +1 ):
+        if haystack[i:i+len(needle)] == needle:
+            return i
+    return -1
+
+
+
+
+
