68d358e9a234a7f8026208c3909994c9fc8b1071
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-01-19 19:07:01 -0600

    Update merge_sorted_array.py

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -13,7 +13,17 @@ nums2 = [2,5,6],       n = 3
 Output: [1,2,2,3,5,6]
 """
 
-def merge(num1,m, num2,n):
+# Time O(m+n); Space O91)
+def merge(nums1,m, nums2,n):
+    while m>0 and n>0 :
+        if nums1[m-1] >= nums2[n-1]:
+            nums1[m+n-1] = num1[m-1]
+            m -= 1
+        else:
+            nums1[m+n-1] = nums2[n-1]
+            n-1
+    if n>0 :
+        nums1[:n] = nums2[:n]
 
 
 
