0dfacbad97ca807bca20de87b389a20018168a52
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-01-24 23:28:05 -0600

    Update longest_palindrome_subsequence.py

--- longest_palindrome_subsequence.py
+++ longest_palindrome_subsequence.py
@@ -19,5 +19,15 @@ One possible longest palindromic subsequence is "bb".
 """
 
 
-
+# <ref> https://www.geeksforgeeks.org/longest-palindromic-subsequence-dp-12/
+# A Dynamic Programming based Python program for LPS problem
+# Return the length of the longest palindromic subsequence in seq
+def lps(str):
+  n = len(str)
+  # create a table to store results of subproblems
+  L = [ [0 for _ in range(n)] for _ in range(n) ]
+  
+  # strings of length 1 are palindrome of length 1; diagonal
+  for i in range(n):
+    L[i][i] = 1
 
