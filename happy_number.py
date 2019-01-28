d71ad5df28d4f40cab3e200e1cb4716f0a0d8bc3
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-01-28 02:11:42 -0600

    Update happy_number.py

--- happy_number.py
+++ happy_number.py
@@ -19,3 +19,18 @@ Explanation:
 """
 
 
+def isHappy(n):
+    mem = set()
+    while n! = 1:
+        n = sum[int(i)**2 for i in str(n)]
+        if n in mem:
+            return False
+        else:
+            mem.add(n)
+    else:
+        return True
+    
+    
+    
+    
+    
