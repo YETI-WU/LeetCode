1f6a4eb309000f185d1e60bf44bb97e05e89daf2
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-02-04 16:26:35 -0600

    Update roman_to_integer.py

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -51,4 +51,18 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
 
 
+# Time O(N); Space O(1)
+def romanToInt(s):
+    roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    res = 0
+    for i in range(0:len(s)-1):
+        if roman[s[i]] < roman[s[i+1])]
+           res -= roman[s[i]]
+        else:
+            res += roman[s[i]]
+    return res + roman[s[-1]]
+
+
+
+
 
