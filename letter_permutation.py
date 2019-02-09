c54932838a95d45c864b4325ba5bb793a6b9456a
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-02-09 03:28:53 -0600

    Update letter_permutation.py

--- letter_permutation.py
+++ letter_permutation.py
@@ -20,11 +20,11 @@ Note:
 
 def letterCasePermutation(S):
     res = ['']
-        for ch in S:
-            if ch.isalpha():
-                res = [ i+j   for i in res for j in [ch.upper(), ch.lower()] ]
-            else:
-                res = [ i+chr for i in res ]
+    for ch in S:
+        if ch.isalpha():
+            res = [ i+j   for i in res for j in [ch.upper(), ch.lower()] ]
+        else:
+            res = [ i+ch for i in res ]
     return res
 
 
