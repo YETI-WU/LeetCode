ee39843ca0c1ab345bb1735fb0ba483d24ab8559
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-02-12 12:47:29 -0600

    Update anagram_valid.py

--- anagram_valid.py
+++ anagram_valid.py
@@ -14,5 +14,11 @@ Note:
 You may assume the string contains only lowercase alphabets.
 """
 
-def isAnagram(s):
+def isAnagram(s, t):
+    dic_s, dic_t = {}, {}
+    for char in s:
+        dic_s[char] = dic_s.get(char, 0) + 1
+    for char in t:
+        dic_t[char] = dic_t.get(char, 0) + 1
+    return dic_s == dic_t
 
