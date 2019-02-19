3abf80c43a031f800e8fdb52010310dceeddf355
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-02-19 07:15:53 -0600

    Update my_sqrt.py

--- my_sqrt.py
+++ my_sqrt.py
@@ -25,10 +25,20 @@ def mySqrt(x):
     return r
 
 
-# Binary Search
+# Binary Search, Time: O(logN)
 def mySqrt(x):
     l , r = 0 , x
     while l <= r:
         mid = (l+r) //2
-        if mid * mid <= x < (mid+1) * (mid+1)
-        
+        if mid * mid <= x < (mid+1) * (mid+1):
+            return mid
+        elif x < mid * mid:
+            r = mid
+        else:
+            l = mid+1
+    
+
+
+
+    
+    
