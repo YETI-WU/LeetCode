9c664cdf6f939bbe9b796cb92c2fc428b0c1d519
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2019-02-22 18:22:02 -0600

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -25,3 +25,23 @@ Return:
     "FizzBuzz"
 ]
 """
+
+def fuzzBuzz(n):
+    res = []
+    for i in range(1,n+1):
+        if i%3 == 0 and i%5 == 0:
+            res.append('FizzBuzz')
+        elif i%3 == 0:
+            res.append('Fizz')
+        elif i%5 == 0:
+            res.append('Buzz')
+        else:
+            res.append(i)
+    return res
+
+
+
+
+
+            
+    
